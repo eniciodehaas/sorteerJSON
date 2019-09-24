2982252096f831d12b6c6cd265740c4087a02a5a
eniciodehaas <43574607+eniciodehaas@users.noreply.github.com>
2019-09-24 17:09:59 +0200

    Add files via upload

--- sorteer.js
+++ sorteer.js
@@ -1,14 +1,9 @@
-// keuze sorteerkenmerk
-let kenmerk = document.getElementById('kenmerk');
-kenmerk.addEventListener('change', (e) => {
-    sorteerObject.sorteerkenmerk = e.target.value;
-    sorteerObject.sorteren();
-})
 // JSON importeren
 let xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         sorteerObject.data = JSON.parse(this.responseText);
+        sorteerObject.voegJSdatumIn();
         sorteerObject.sorteren();
     } 
 }
@@ -39,6 +34,52 @@ const maakTabelRij = (arr, accent) => {
     return rij;
 }
 
+// zet maand woord op in nummer
+// waarbij januari 0 geeft
+// en december 11
+const geefMaandNummer = (maand) => {
+    let nummer;
+    switch (maand) {
+        case "januari":       nummer = 0; break;
+        case "februari":      nummer = 1; break;
+        case "maart":         nummer = 2; break;
+        case "april":         nummer = 3; break;
+        case "mei":           nummer = 4; break;
+        case "juni":          nummer = 5; break;
+        case "juli":          nummer = 6; break;
+        case "augustus":      nummer = 7; break;
+        case "september":     nummer = 8; break;
+        case "oktober":       nummer = 9; break;
+        case "november":      nummer = 10; break;
+        case "december":      nummer = 11; break;
+        default:              nummer = 0;
+    }
+    return nummer;
+}
+
+// zet datum string op in datum-object
+const maakDatum = (maandJaar) => {
+    let mjArray = maandJaar.split(" ");
+    let datum = new Date(mjArray[1], geefMaandNummer(mjArray[0]));
+    return datum;
+}
+
+
+// functie maakt van een array een opsomming met ', ' en ' en '
+const maakOpsomming = (array) => {
+    let string = "";
+    for (let i = 0; i < array.length; i++) {
+        switch (i) {
+            case array.length-1: string += array[i]; break;
+            case array.length-2: string += array[i] + " en "; break;
+            default: string += array[i] + ", "
+        }
+        
+    }
+    return string;
+}
+
+
 
 // maak object dat de boeken uitvoert en sorteert en data bevat
 // eigenschappen: data en sorteerkenmerk
@@ -48,8 +89,18 @@ let sorteerObject = {
 
     sorteerkenmerk: "titel",
 
+    // sorteervolgorde
+    sorteerVolgorde: 1,
+
+    // datum object toevoegen aan data vanuit "uitgave"
+    voegJSdatumIn: function() {
+        this.data.forEach((item) => {
+           item.jsDatum = maakDatum(item.uitgave);
+        });
+    },
+
     sorteren: function() {
-        this.data.sort( (a,b) => a[this.sorteerkenmerk] > b[this.sorteerkenmerk] ? 1 : -1 );
+        this.data.sort( (a,b) => a[this.sorteerkenmerk] > b[this.sorteerkenmerk] ? 1*this.sorteerVolgorde : -1*this.sorteerVolgorde );
         this.uitvoeren(this.data);
     },
 
@@ -73,9 +124,11 @@ let sorteerObject = {
             "' class='boeken__cover' alt='" + 
             data[i].titel + 
             "'>";
+            // maak opsomming van auteurs
+            let auteurs = maakOpsomming(data[i].auteur);
             uitvoer += maakTabelRij(
                 [data[i].titel, 
-                data[i].auteur[0], 
+                auteurs, 
                 imgElement, 
                 data[i].uitgave, 
                 data[i].paginas, 
@@ -88,3 +141,16 @@ let sorteerObject = {
         document.getElementById('uitvoer').innerHTML = uitvoer;
     }
 }
+
+// keuze sorteerkenmerk
+document.getElementById('kenmerk').addEventListener('change', (e) => {
+    sorteerObject.sorteerkenmerk = e.target.value;
+    sorteerObject.sorteren();
+})
+
+document.getElementsByName('oplopend').forEach((item) => {
+    item.addEventListener('click', (e) => {
+        sorteerObject.sorteerVolgorde = parseInt(e.target.value);
+        sorteerObject.sorteren();
+    })
+})
